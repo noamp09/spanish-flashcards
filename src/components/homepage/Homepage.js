4a16503dbad6efc3c19d8289750f1e48d86b895d
noamp09 <noamp09@gmail.com>
2024-07-06 18:15:40 +0300

    built the home screen

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,17 +1,43 @@
 import './HomePage.css'
+import logo from '../../assets/logo.svg';
+import flag from '../../assets/flag.svg'
+
 
 function HomePage() {
   return (
-    <div className='vertical-container'>
-      <h1>Welcome to our Spanish Learning Website!</h1>
-      <p>
-        Start your journey to expand your Spanish vocabulary with us. Whether you are a beginner
-        or looking to improve your skills, we have resources for you.
-      </p>
-      <p>Ready to get started? {' '}
-        <a href='/flashcard'>Go To Flashcards</a>
-      </p>
-    </div>
+    <>
+  <div id="top">
+          <nav id="header">
+              <a id='logo' href='/'><img src={logo} alt='logo'/>{' '}Tucabulario</a>
+              <a href='/flashcard'><button className='btn'>Go Practicing</button></a>
+          </nav>
+
+          {/* <div class="hero">
+                  <h2 class="main-text">Welcome to our Spanish Learning Website!</h2>
+                  <div class="secondary-text"> Start your journey to expand your Spanish vocabulary with us. Whether you are a beginner 
+                    or looking to improve your skills, we have resources for you.</div>
+          </div> */}
+          <div id='hero'>
+            <img id='flag' src={flag} alt='Spanish Flag' />
+            <div id='entertaining'>An entertaining and interactive way to expand your vocabulary!</div>
+          </div>
+      </div>
+
+      <div id="topics">
+          <h1>Topics / Temas</h1>
+          <div id='horizontal-container'>
+            <a href='top-200' className='topic'>top 200</a>
+            <a href="animals" className='topic'>animals</a>
+            <a href="colors" className='topic'>colors</a>
+          </div>
+      </div>
+
+          <div id='quote'>
+            <h1>stay consistent and see the results</h1>
+            <div id='sentence'>La excelencia no es un acto, es un hábito.</div>
+          </div>
+
+      </>
   );
 }
 
